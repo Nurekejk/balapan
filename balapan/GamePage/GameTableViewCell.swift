795bb618d8530b347d5aa515ca7b553ce6fe8205
Nurbol Zhetpisbay <zhetpisbaynurbol@gmail.com>
2024-04-04 16:15:53 +0500

    [BA - 5] Сверстал экран GamePage

---
+++ balapan/GamePage/GameTableViewCell.swift
@@ -0,0 +1,124 @@
+//
+//  GameTableViewCell.swift
+//  balapan
+//
+//  Created by Nurbol on 29.03.2024.
+//
+
+import UIKit
+
+class GameTableViewCell: UITableViewCell {
+
+    private let mainContainer: UIView = {
+        let uiView = UIView()
+        uiView.backgroundColor = UIColor(red: 241/255, green: 174/255, blue: 22/255, alpha: 1)
+        uiView.layer.cornerRadius = 20
+        return uiView
+    }()
+
+    
+    private let gameCategory: UILabel = {
+        let label = UILabel()
+        label.text = "Әріптер"
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 20, weight: .semibold)
+        return label
+    }()
+
+    private let playContainer: UIView = {
+        let uiView = UIView()
+        uiView.backgroundColor = UIColor(red: 83/255, green: 36/255, blue: 242/255, alpha: 1)
+        uiView.layer.cornerRadius = 15
+        return uiView
+    }()
+
+    private let label: UILabel = {
+        let label = UILabel()
+        label.text = "Play"
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 12, weight: .semibold)
+        return label
+    }()
+    private let playIcon: UIImageView = {
+        let playIcon = UIImageView()
+        playIcon.image = UIImage (named: "playIcon")
+        return playIcon
+    }()
+    private let categoryImage: UIImageView = {
+        let categoryImage = UIImageView()
+        categoryImage.image = UIImage (named: "gameImage")
+        return categoryImage
+    }()
+
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupHierarchy()
+        setupLayout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupHierarchy() {
+        contentView.addSubview(mainContainer)
+        mainContainer.addSubview(gameCategory)
+        mainContainer.addSubview(playContainer)
+        mainContainer.addSubview(categoryImage)
+        playContainer.addSubview(label)
+        playContainer.addSubview(playIcon)
+
+
+
+    }
+
+    private func setupLayout () {
+        mainContainer.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(8)
+            make.bottom.equalToSuperview().offset(-8)
+            make.leading.trailing.equalToSuperview()
+        }
+        gameCategory.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(32)
+            make.leading.equalToSuperview().offset(24)
+        }
+        categoryImage.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview().offset(-18)
+            make.width.equalTo(128)
+            make.height.equalTo(109)
+        }
+        playContainer.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(77)
+            make.leading.equalToSuperview().offset(20)
+            make.width.equalTo(66)
+            make.height.equalTo(28)
+        }
+        playIcon.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview().offset(10)
+            make.trailing.equalToSuperview().offset(-42)
+        }
+        label.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(30)
+            make.trailing.equalToSuperview().offset(-11)
+            make.centerY.equalToSuperview()
+        }
+    }
+
+
+}
+
+
+
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
--- balapan/GamePage/GameViewController.swift
+++ balapan/GamePage/GameViewController.swift
@@ -9,21 +9,51 @@ import UIKit
 
 class GameViewController: UIViewController {
 
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.register(GameTableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.separatorStyle = .none
+        return tableView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        setupViews()
+        setupConstraints()
 
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
+    private func setupViews(){
+        view.addSubview(tableView)
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    } 
+    private func setupConstraints(){
+        tableView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(112)
+            make.leading.equalToSuperview().offset(12)
+            make.trailing.equalToSuperview().offset(-12)
+            make.height.equalToSuperview()
+        }
     }
-    */
 
+
+}
+
+extension GameViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell",
+                                                 for: indexPath) as? GameTableViewCell
+        return cell ?? UITableViewCell()
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        125+16
+    }
 }
